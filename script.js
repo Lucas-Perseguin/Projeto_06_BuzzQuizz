d85f9840861ab277ade6920aa676db5189c0c322
Lucas-Perseguin <lucasperseguin@gmail.com>
2022-09-04 22:30:20 -0300

    fix: html nivel

--- script.js
+++ script.js
@@ -337,7 +337,9 @@ function criarElementoPergunta(numeroPergunta) {
     const perguntaMinimizada = document.createElement('div');
     const pergunta = document.createElement('div');
     perguntaMinimizada.classList.add('pergunta-minimizada', 'display-pergunta-minimizada');
+    perguntaMinimizada.setAttribute('data-identifier', 'expand');
     pergunta.classList.add('pergunta', 'escondido');
+    pergunta.setAttribute('data-identifier', 'question-form');
     perguntaMinimizada.innerHTML = `<h2>Pergunta ${numeroPergunta}</h2>
     <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M18.1594 15.4969L19.6038 14.0594C19.8295 13.8348 20.2222 13.992 20.2222 14.3155V20.8471C20.2222 22.0375 19.2517 23.0034 18.0556 23.0034H2.16667C0.970486 23.0034 0 22.0375 0 20.8471V5.03462C0 3.84419 0.970486 2.87837 2.16667 2.87837H14.5122C14.8326 2.87837 14.9951 3.2647 14.7694 3.4938L13.325 4.9313C13.2573 4.99868 13.167 5.03462 13.0677 5.03462H2.16667V20.8471H18.0556V15.7485C18.0556 15.6542 18.0917 15.5643 18.1594 15.4969ZM25.2281 6.43169L13.3747 18.2282L9.2941 18.6774C8.11146 18.8077 7.10486 17.8149 7.23576 16.629L7.68715 12.568L19.5406 0.771533C20.5743 -0.257178 22.2444 -0.257178 23.2736 0.771533L25.2236 2.71216C26.2573 3.74087 26.2573 5.40747 25.2281 6.43169ZM20.7684 7.81978L18.1458 5.20981L9.75903 13.5608L9.42951 16.4942L12.3771 16.1663L20.7684 7.81978ZM23.6934 4.2395L21.7434 2.29888C21.5583 2.1147 21.2559 2.1147 21.0753 2.29888L19.6806 3.68696L22.3031 6.29692L23.6979 4.90884C23.8785 4.72017 23.8785 4.42368 23.6934 4.2395Z" fill="black"/>
@@ -352,16 +354,16 @@ function criarElementoPergunta(numeroPergunta) {
     <input type="text" placeholder="Resposta correta" class="resposta-correta">
     <input type="url" class="url" placeholder="URL da imagem">
     <h2>Respostas incorretas</h2>
-    <div class="respostas-incorretas display-tela-3" class="resposta-incorreta">
-        <input type="text" placeholder="Resposta incorreta 1">
+    <div class="respostas-incorretas display-tela-3">
+        <input type="text" placeholder="Resposta incorreta 1" class="resposta-incorreta">
         <input type="url" class="url" placeholder="URL da imagem 1">
     </div>
-    <div class="respostas-incorretas display-tela-3" class="resposta-incorreta">
-        <input type="text" placeholder="Resposta incorreta 2">
+    <div class="respostas-incorretas display-tela-3">
+        <input type="text" placeholder="Resposta incorreta 2" class="resposta-incorreta">
         <input type="url" class="url" placeholder="URL da imagem 2">
     </div>
-    <div class="respostas-incorretas display-tela-3" class="resposta-incorreta">
-        <input type="text" placeholder="Resposta incorreta 3">
+    <div class="respostas-incorretas display-tela-3">
+        <input type="text" placeholder="Resposta incorreta 3" class="resposta-incorreta">
         <input type="url" class="url" placeholder="URL da imagem 3">
     </div>`;
     perguntaToda.appendChild(perguntaMinimizada);
@@ -391,7 +393,7 @@ function validarPerguntasQuizz() {
     const qtdePerguntas = document.getElementById('qtdePerguntasCriarQuizz').value;
     const perguntas = document.querySelectorAll('.tela-3-2 .pergunta');
     let perguntasBoolean = false;
-    for (let i = 0; i < perguntas.length; i++) {
+    for (let i = 0; i < qtdePerguntas; i++) {
         perguntasBoolean = validarPerguntaQuizz(perguntas[i]);
         if (!perguntasBoolean) {
             alert('Há algo de errado em uma de suas perguntas, por favor verifique e corrija o erro!');
@@ -503,12 +505,14 @@ function criarElementoNivel(numeroNivel) {
     nivelTodo.setAttribute('onclick', 'selecionarNivelCriacaoQuizz(this)');
     const nivelMinimizado = document.createElement('div');
     nivelMinimizado.classList.add('nivel-minimizado', 'display-nivel-minimizado');
+    nivelMinimizado.setAttribute('data-identifier', 'expand');
     nivelMinimizado.innerHTML = `<h2>Nível ${numeroNivel}</h2>
     <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M18.1594 15.4969L19.6038 14.0594C19.8295 13.8348 20.2222 13.992 20.2222 14.3155V20.8471C20.2222 22.0375 19.2517 23.0034 18.0556 23.0034H2.16667C0.970486 23.0034 0 22.0375 0 20.8471V5.03462C0 3.84419 0.970486 2.87837 2.16667 2.87837H14.5122C14.8326 2.87837 14.9951 3.2647 14.7694 3.4938L13.325 4.9313C13.2573 4.99868 13.167 5.03462 13.0677 5.03462H2.16667V20.8471H18.0556V15.7485C18.0556 15.6542 18.0917 15.5643 18.1594 15.4969ZM25.2281 6.43169L13.3747 18.2282L9.2941 18.6774C8.11146 18.8077 7.10486 17.8149 7.23576 16.629L7.68715 12.568L19.5406 0.771533C20.5743 -0.257178 22.2444 -0.257178 23.2736 0.771533L25.2236 2.71216C26.2573 3.74087 26.2573 5.40747 25.2281 6.43169ZM20.7684 7.81978L18.1458 5.20981L9.75903 13.5608L9.42951 16.4942L12.3771 16.1663L20.7684 7.81978ZM23.6934 4.2395L21.7434 2.29888C21.5583 2.1147 21.2559 2.1147 21.0753 2.29888L19.6806 3.68696L22.3031 6.29692L23.6979 4.90884C23.8785 4.72017 23.8785 4.42368 23.6934 4.2395Z" fill="black"/>
     </svg>`;
     const nivel = document.createElement('div');
     nivel.classList.add('nivel', 'escondido');
+    nivel.setAttribute('data-identifier', 'level')
     nivel.innerHTML = `<h2>Nível ${numeroNivel}</h2>
     <input type="text" placeholder="Título do nível" class="titulo-nivel">
     <input type="number" min="0" max="100" placeholder="% de acerto mínimo" class="acerto-nivel">
